e52d63031b878b66eb70ae8f17c9997ca5d26a8f
Nguyen Nhat Hao <nguyennhathao@yandex.com>
2022-08-05 12:01:16 +0700

    feat(lsp): add lua language server

--- lua/lsp/language-servers/init.lua
+++ lua/lsp/language-servers/init.lua
@@ -4,7 +4,7 @@ local fmt = require("lsp-format")
 fmt.setup {}
 lsp.tsserver.setup(coq.lsp_ensure_capabilities({
 	on_attach = fmt.on_attach,
-  root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
+	root_dir = require("lspconfig").util.root_pattern("package.json", "tsconfig.json", "jsconfig.json"),
 }))
 -- lsp.emmet_ls.setup({
 -- 	capabilities = capabilities,
@@ -12,6 +12,29 @@ lsp.tsserver.setup(coq.lsp_ensure_capabilities({
 -- 	filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css' },
 -- })
 
+lsp.sumneko_lua.setup {
+	on_attach = fmt.on_attach,
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { 'vim' },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+}
 
 lsp.gopls.setup(coq.lsp_ensure_capabilities())
 vim.cmd [[
@@ -19,7 +42,7 @@ let g:go_def_mode='gopls'
 let g:go_info_mode='gopls'
 ]]
 vim.g.markdown_fenced_languages = {
-  "ts=typescript"
+	"ts=typescript"
 }
 
 -- lsp.denols.setup({
@@ -29,11 +52,11 @@ vim.g.markdown_fenced_languages = {
 
 lsp.volar.setup(coq.lsp_ensure_capabilities({
 	on_attach = fmt.on_attach,
-  init_options = {
-    typescript = {
-		  serverPath = "/Users/kaos/Library/pnpm/global/5/node_modules/typescript/lib/tsserverlibrary.js"
-    },
-  },
+	init_options = {
+		typescript = {
+			serverPath = "/Users/kaos/Library/pnpm/global/5/node_modules/typescript/lib/tsserverlibrary.js"
+		},
+	},
 }))
 
 lsp.svelte.setup(coq.lsp_ensure_capabilities({
@@ -42,7 +65,8 @@ lsp.svelte.setup(coq.lsp_ensure_capabilities({
 
 lsp.tailwindcss.setup(coq.lsp_ensure_capabilities({
 	on_attach = fmt.on_attach,
-	root_dir = require("lspconfig").util.root_pattern('tailwind.config.js', 'tailwind.config.ts', 'unocss.config.js', 'unocss.config.ts')
+	root_dir = require("lspconfig").util.root_pattern('tailwind.config.js', 'tailwind.config.ts', 'unocss.config.js',
+		'unocss.config.ts')
 }))
 
 lsp.dartls.setup(coq.lsp_ensure_capabilities({
