10ddc8cadba0b427393d144311ae9f34051a58dd
Nguyen Nhat Hao <nguyennhathao@yandex.com>
2024-12-05 05:14:39 +0700

    update: add mini.pairs

--- lua/plug-ins/init.lua
+++ lua/plug-ins/init.lua
@@ -8,6 +8,7 @@ add('echasnovski/mini.files')
 add('echasnovski/mini.pick')
 add('echasnovski/mini.move')
 add('echasnovski/mini.notify')
+add('echasnovski/mini.pairs')
 
 -- code
 add('williamboman/mason.nvim')
