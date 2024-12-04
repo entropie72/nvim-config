5a3fb94fbe3b581b138b0d31c59ba3eb5c87f3fb
Nguyen Nhat Hao <nguyennhathao@yandex.com>
2024-12-05 05:28:55 +0700

    update(treesitter): regular update

--- lua/code/treesitter/init.lua
+++ lua/code/treesitter/init.lua
@@ -1,6 +1,14 @@
+-- folding
+--[[ vim.wo.foldmethod = 'expr'
+vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()' ]]
+
 require('nvim-treesitter.configs').setup {
 	auto_install = false,
 	indent  = {
 		enable = true
 	},
+
+	incremental_selection = {
+		enable = false
+	}
 }
