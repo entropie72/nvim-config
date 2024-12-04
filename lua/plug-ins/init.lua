2eea6bf6929e72f18bcaee9212ba8d9cab0df7c4
Nguyen Nhat Hao <nguyennhathao@yandex.com>
2024-12-04 16:06:34 +0700

    update: add mini.notify

--- lua/plug-ins/init.lua
+++ lua/plug-ins/init.lua
@@ -2,9 +2,9 @@ require('plug-ins.deps').setup()
 local add = MiniDeps.add
 
 -- editor
-add('Shatur/neovim-ayu')
 add('EdenEast/nightfox.nvim')
 add('echasnovski/mini.icons')
 add('echasnovski/mini.files')
 add('echasnovski/mini.pick')
 add('echasnovski/mini.move')
+add('echasnovski/mini.notify')
