a21c6a7ff4c8e19e2e5aacc454c61a10ae9e9119
Nguyen Nhat Hao <nguyennhathao@yandex.com>
2025-01-14 21:14:08 +0700

    update(lsp): add emmet (for tsx)

--- lua/code/lsp/html_css/init.lua
+++ lua/code/lsp/html_css/init.lua
@@ -6,6 +6,10 @@ require 'lspconfig'.html.setup {
     capabilities = capabilities,
 }
 
+require 'lspconfig'.emmet_language_server.setup({
+    filetypes = { "html", "javascriptreact", "typescriptreact", "typescript.tsx" }
+})
+
 require 'lspconfig'.cssls.setup {
     capabilities = capabilities,
 }
